f236187016393fadc3b1f17dacae3bd93b972b6f
mikey dagitses <mikeyd@meta.com>
2023-04-09 19:22:35 -0700

    move cmake configure_file emulation into a python file
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98651
    
    This allows us to no longer need to worry about whether any of the
    build system's genrules or rule implementations want a shell parseable
    command or an exec style list of arguments.
    ghstack-source-id: 185491464
    
    Differential Revision: [D44809219](https://our.internmc.facebook.com/intern/diff/D44809219/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Meta-specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D44809219/)!

--- c10/macros/cmake_configure_file/tool.py
+++ c10/macros/cmake_configure_file/tool.py
@@ -1,4 +1,17 @@
-def cmake_configure_file_cmd(template_path, definitions, output_path):
+from __future__ import annotations
+
+import pathlib
+import subprocess
+import sys
+
+def main(argv: list[str]) -> None:
+    template_path = sys.argv[1]
+    output_path = pathlib.Path(sys.argv[2])
+    definitions = list(sys.argv[3:])
+    subprocess.run(" ".join(cmake_configure_file_cmd(template_path, definitions, output_path)),
+                   check=True, shell=True)
+
+def cmake_configure_file_cmd(template_path: str, definitions: list[str], output_path: pathlib.Path):
     command = ["sed", "--regexp-extended"]
     for definition in definitions:
         command.append(
@@ -24,3 +37,7 @@ def cmake_configure_file_cmd(template_path, definitions, output_path):
     command.append("> {}".format(output_path))
 
     return command
+
+
+if __name__ == "__main__":
+    main(sys.argv)
